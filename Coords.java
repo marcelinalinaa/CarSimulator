e0b382fe655867cfd8f740904ee0a3987de759bd
vanessaeca <75422685+vanessaeca@users.noreply.github.com>
2020-12-05 20:56:11 +0700

    Update Coords.java

--- Coords.java
+++ Coords.java
@@ -1,18 +1,34 @@
 package carSimulator;
 
 public class Coords {
-public int x;
-public int y;
-public Coords(int x, int y){
-x = this.x;
-y = this.y;
-}
-public boolean equals(Object o) {
-Coords c = (Coords) o;
-return c.x == x && c.y == y;
-}
-/*public int hashCode() {
-return new Integer(x + "0" + y);
-}
-*/
+	private int x;
+	private int y;
+	
+	public Coords(int x, int y){
+		setCoords(x,y);
+	}
+	
+	public Coords() {
+		setCoords(0,0);
+	}
+	
+	public boolean equals(Object o) {
+		Coords c = (Coords) o;
+		return c.x == x && c.y == y;
+	}
+	
+	public void setCoords(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getCoordsX() {
+		return this.x;
+	}
+	
+	public int getCoordsY() {
+		return this.y;				
+	}
+	
+	
 }

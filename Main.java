6c569656971609d981c89a27b0202fed5f75f822
marcelinalinaa <74171460+marcelinalinaa@users.noreply.github.com>
2020-12-05 18:48:50 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,42 @@
+
+package CarSimulator;
+
 public class Main {
 
-    public static void main(String[] args) {
+ static RoadMap map = new RoadMap();
+ static char[][] road = map.getRoad();
+ 
+    public static void main(String[] args){
         
+       
         printMap();
-        
+        slowdown();
+        clearScreen(); 
+        /*
+            while()
+
+
+        */
     }
-    
     public static void printMap(){
-        
-        RoadMap map = new RoadMap();
-        char[][] road;
-        road = map.getRoad();
-        
+    
         for(int i = 0; i <15;i++) {
             for(int j = 0; j <35; j++) {
-                System.out.print(road[i][j]);
-            }
-        System.out.println();
+            System.out.print(road[i][j]);
+        }
+            System.out.println();
         }
     }
+    public static void clearScreen() {   
+       System.out.print("\033[H\033[2J");   
+        System.out.flush();   
+       } 
+    public static void slowdown(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+   
+}

76b62d6060a23076ad8d140db2fb02d213513a45
marcelinalinaa <74171460+marcelinalinaa@users.noreply.github.com>
2020-12-06 11:24:19 +0700

    Update RoadMap.java

--- RoadMap.java
+++ RoadMap.java
@@ -34,51 +34,84 @@ public class RoadMap {
 	   
 	
 	   public RoadMap(){
-		   setObstacle();
+		   setObstacleCone();
+		   setTrafficLight();
+		   setParkSignAndDestination();
 	   }
 		   
 	   public char[][] getRoad(){
 		     return road;
 	   }
 	   
-	   public void setObstacle(){
-	        int count = 0;
-	        int a,b;
-
-
-	        while(count<2){
-	            a = (int) Math.floor(Math.random() * 15);
-	            b = (int) Math.floor(Math.random() * 35);
-	            if(road[a][b] == ' '){
-	               road[a][b] = 'C';
-	            count++;
-	            }
-	        }
-	        count = 0;
-	        while(count<1){
-	            a = (int) Math.floor(Math.random() * 15);
-	            b = (int) Math.floor(Math.random() * 35);
-	            if(road[a][b] == '#'){
-	                road[a][b] = 'P';
-	            count++;
-	            }
-	        }
-	    
-	        count = 0;
-	        while(count<1){
-	            a = (int) Math.floor(Math.random() * 15);
-	            b = (int) Math.floor(Math.random() * 35);
-	            if(road[a][b] == '#'){
-	                road[a][b] = 'R';
-	            count++;
-	            }
-	        }
-
+	   public void setParkSignAndDestination(){
+			int a,b;
+			int count = 0;
+			int type = getRandomInt(0, 2);
+			// kalo type == 0, berarti tanda parkir dan tujuan berada di ujung kanan map
+			if(type == 0){
+				while(count<1){
+					a = getRandomInt(0, 5); //baris ke 1-3
+					b = MAP_WIDTH-1;		//kolom terakhir
+					if(road[a][b] == '#'){
+						road[a][b] = 'P';
+						if(a==0){
+							road[a+1][b] = 'X';
+						}
+						else{ road[a-1][b]= 'X';}
+					count++;
+					}
+				}
+			}
+			else{ // berarti tanda parkir dan tujuan berada di bagian bawah map
+				while(count<1){
+					a = MAP_HEIGHT-1;		//baris terakhir
+					b = getRandomInt(14, 19);		//kolom terakhir
+					if(road[a][b] == '#'){
+						road[a][b] = 'P';
+						if(b==14){
+							road[a][b+1] = 'X';
+						}
+						else{ road[a][b-1]= 'X';}
+					count++;
+					}
+				}
+			}
+	        
 	    }
 	   
 	   public char getPoint(int x, int y){
 	     return road[x][y];
 	   }
+
+	   public void setObstacleCone(){
+			int count = 0;
+			int a,b;
+			while(count<2){
+				a = getRandomInt(0, MAP_HEIGHT);
+				b = getRandomInt(0, MAP_WIDTH);
+				if(road[a][b] == ' '){
+				road[a][b] = 'C';
+				count++;
+				}
+			}
+	   }
+	   public void setTrafficLight(){
+		   	int a,b;
+			int count = 0;
+			while(count<1){
+				a = getRandomInt(0, MAP_HEIGHT);
+				b = getRandomInt(0, MAP_WIDTH);
+				if(road[a][b] == '#'){
+					road[a][b] = 'R';
+				count++;
+				}
+			}
+	   }
+
+	   public void setPoint(int x,int y, char input){
+		   this.road[x][y] = input;
+	   }
+
 	   
 //	   public List<Coords> startingPoints() {
 //		
@@ -96,6 +129,8 @@ public class RoadMap {
 //		  carStartingPoint = startingPoints.get(index);
 //	   }
 //	   
-
+		public int getRandomInt(int min, int max) {
+			return (int) Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+		}
 	   
 }
